447c6ac990e4d76a48cd6035e3a9ccfd4bc1b91f
lukerhys <hello@lukemelvin.com>
2020-02-04 16:01:51 +0000

    MVP complete basic styling added

---
+++ homework_cinema_advanced/controller.rb
@@ -0,0 +1,11 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require('pry-byebug')
+
+require_relative('./models/film')
+also_reload('./models/*')
+
+get "/films" do
+@films = Film.all()
+  erb(:index)
+end
